c3f0fcc1bcda498035faa133f06130c3e9a9744f
Chad Milando <44262671+cmilando@users.noreply.github.com>
2024-05-24 08:41:48 -0400

    Update test_real.R

--- test_real.R
+++ test_real.R
@@ -11,7 +11,6 @@ caseCounts <- create_caseCounts(sample_dates, sample_location, sample_cases)[1:8
 ll <- convert_to_linelist(caseCounts, reportF_missP = 0.01)
 ll <- data.frame(ll) %>% filter(!is.na(delay_int))
 
-
 head(ll)
 
 ll$value <- 1
@@ -19,7 +18,8 @@ ll$id <- 1:nrow(ll)
 ll <- data.frame(ll)
 Y <- ll$delay_int
 
-ll$start_dt <- as.vector(ll$onset_date - min(ll$onset_date))
+## THIS SHOULD BE DEFINED BY REPORTING DATE
+ll$start_dt <- as.vector(ll$report_date - min(ll$report_date))
 
 xx <- ll %>%
   mutate(actual_onset = start_dt,
@@ -27,10 +27,6 @@ xx <- ll %>%
 
 rr <- xx %>% group_by(actual_report) %>% tally()
 oo <- xx %>% group_by(actual_onset) %>% tally()
-
-
-
-## week is defined by the 
 
 dt_wide <- ll %>% arrange(onset_date) %>%
   pivot_wider(id_cols = c(id, onset_date, is_weekend),
@@ -56,17 +52,19 @@ out <- rstan::extract(mod1)
 
 summary(out$phi)
 
-
 dim(out$y_rep)
 out$y_rep[1:5, 1:5]
 
 out1 <- lapply(1:nrow(out$y_rep), function(i) {
-xx1$pred_delay = out$y_rep[i, ]
-xx1 <- xx1 %>%
-  mutate(pred_onset = actual_report - pred_delay)
-xx1 %>% group_by(pred_onset) %>% tally() %>% mutate(iter = i)
+  xx1 <- xx
+  xx1$pred_delay = out$y_rep[i, ]
+  xx1 <- xx1 %>%
+    mutate(pred_onset = actual_report - pred_delay)
+  xx1 %>% group_by(pred_onset) %>% tally() %>% mutate(iter = i)
 })
+
 out_df <- do.call(rbind, out1)
+
 summaryx <- out_df %>%
   group_by(pred_onset) %>%
   summarize(
@@ -77,9 +75,15 @@ summaryx <- out_df %>%
   )
 head(summaryx)
 
-
 plot(rr)
 lines(oo, col='blue')
 lines(x = summaryx$pred_onset, y = summaryx$lb, col='green')
 lines(x = summaryx$pred_onset, y = summaryx$ub, col='green')
 lines(x = summaryx$pred_onset, y = summaryx$ex, col='red')
+legend("topright", 
+       legend = c("Reported cases", "Observed Onset", "Lower Bound", 
+                  "Upper Bound", "Predicted Onset"), 
+       col = c("black", "blue", "green", "green", "red"), 
+       lty = c(NA, 1, 1, 1, 1), # Line types
+       pch = c(1, NA, NA, NA, NA), # Point types (1 is a default point type)
+       cex = 0.8) # Text size
